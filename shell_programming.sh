919c3eb828335c7c36c95bbbd600f43d70e6d18c
roberts.mukans@gmail.com <timarti-5@inbox.lv>
2017-09-27 10:09:45 +0300

    20170927_10_10

---
+++ shell_programming.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAME="Vards Uzvards"
+echo NAME
+echo $NAME
+NAME="Vards Uzvards"
+echo $NAME
+NAME=" Vaards Uzvaards"
+readonle NAME
+readonly NAME
+echo $NAME
+NAME="Uzvaards Vaards"
+
