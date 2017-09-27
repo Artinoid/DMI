6ba85f0a6f131bceda1d63cfc73dc99b73187f77
roberts.mukans@gmail.com <timarti-5@inbox.lv>
2017-09-27 12:07:04 +0300

    20170927_12_05

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#4. piemers
+echo "tiek pildit skripts: "$0
+echo $n
+echo "skriptu nodotu orgumentu skaits: "$#
+echo "skriptam nodoti argumenti (attelosana un grupesana 1):"$*
+echo " skriptam nodoti argumenti (attelosana/grupesana 2):"$@
+echo $&
+echo $$
+echo $!
 NAME="Vards Uzvards"
 echo NAME
 echo $NAME
 NAME="Vards Uzvards"
 echo $NAME
-NAME=" Vaards Uzvaards"
-readonle NAME
-readonly NAME
-echo $NAME
-NAME="Uzvaards Vaards"
 
