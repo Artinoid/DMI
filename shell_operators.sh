1dfdd3673e67f760a3b3d98accd703a09a0e9e6b
roberts.mukans@gmail.com <timarti-5@inbox.lv>
2017-10-11 10:00:35 +0300

    20171011_09_55

---
+++ shell_operators.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# 1. piemers - izteiksmes pieraksts
+val1="expr 2+2"
+echo "Rezultats 2+2 (pedinas, bez atstarpem)" $val1
+val1="expr 2+2"
+echo "Rezultats 2 +2 (pedinas, bez atstarpem)" $val1
+val1="expr 2+2"
+echo "Rezultats 2+ 2 (pedinas, bez atstarpem)" $val1
+# 2. piemers - operacihas ar konstantem
+val21=`expr 2 + 3`
+echo "2 + 3 = "$val21
+val22=`expr 2 - 3`
+echo "2 - 3 = "$val22
+val23=`expr 2 \* 3`
+echo "2 * 3 = "$val23
+val24=`expr 2 / 3`
+echo "2 / 3 = "$val24
+val25=`expr 2 % 3`
+echo "2 % 3 = "$val25
+# 3 piemers -  operacijas ar mainigajiem
+: <<'END'
+a=29
+b=15
+val31=`expr $a + $b`
+echo "$a + $b = "$val31
+val32=`expr $a - $b`
+echo "$a - $b = "$val32
+val33=`expr #a * $b`
+echo "$a * $b = "$val33
+val34=`expr $a / $b`
+echo "$a / $b = "$val34
+val35=`expr $a % $b`
+echo "$a % $b = "$val35
+END
+
+
+
+
